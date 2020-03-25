9397b0042bc1754d0e71958494d5b01dd2436a69
rart <royart@me.com>
2020-03-25 17:39:38 +0100

    * Add react app build * Re-enable git add to build script

---
+++ static-assets/app/precache-manifest.6889abfc25514c5db8b793e8289c91de.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9332c812eb8cdc6a416712382421a9",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "e708ef0d6deb00cc10a8",
+    "url": "/static-assets/app/static/js/10.3288d824.chunk.js"
+  },
+  {
+    "revision": "02adcaded1d4085ca80c",
+    "url": "/static-assets/app/static/js/11.e9fe77f1.chunk.js"
+  },
+  {
+    "revision": "296fdaf31109c6761796",
+    "url": "/static-assets/app/static/js/12.2ea84d9f.chunk.js"
+  },
+  {
+    "revision": "b4dbe0b29209c3bcfdc1",
+    "url": "/static-assets/app/static/js/2.63c6d8cc.chunk.js"
+  },
+  {
+    "revision": "f8f2af7ae91e9be4fb78d6cf665b36ab",
+    "url": "/static-assets/app/static/js/2.63c6d8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38571067aa14efb03bd0",
+    "url": "/static-assets/app/static/js/3.afdd5348.chunk.js"
+  },
+  {
+    "revision": "96e46fc20e848960604c",
+    "url": "/static-assets/app/static/js/4.2af34601.chunk.js"
+  },
+  {
+    "revision": "0324c9a16691d7abd210",
+    "url": "/static-assets/app/static/js/5.6db4ce4f.chunk.js"
+  },
+  {
+    "revision": "220020d5a8f75370c0da",
+    "url": "/static-assets/app/static/js/6.00f44d69.chunk.js"
+  },
+  {
+    "revision": "c90f48b564780c860d70",
+    "url": "/static-assets/app/static/js/7.26ae9306.chunk.js"
+  },
+  {
+    "revision": "abf3ed6e3f9b14a72846",
+    "url": "/static-assets/app/static/js/8.dc5bcdf6.chunk.js"
+  },
+  {
+    "revision": "850f57d45a1bd2590212",
+    "url": "/static-assets/app/static/js/9.37492201.chunk.js"
+  },
+  {
+    "revision": "2ae77d3c7545de1694a6",
+    "url": "/static-assets/app/static/js/main.29b3e156.chunk.js"
+  },
+  {
+    "revision": "b0df02cd8101afed22e0",
+    "url": "/static-assets/app/static/js/runtime-main.a63afa43.js"
+  }
+]);
